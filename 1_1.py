df69c90818196cc73c1b405288e814ed82e50c68
Anastasia Shaldova <83241853+AnastasiaShaldova@users.noreply.github.com>
2021-09-05 14:59:11 +0300

    Delete 1_1.py

--- 1_1.py
+++
@@ -1,8 +0,0 @@
-duration = int(input('Введите количество секунд: '))
-second = duration % 60
-minute = (duration // 60) % 60
-hour = (duration // 3600) % 24
-day = (duration // 86400) % 365
-year = duration // 31_536_000
-
-print(f'{year} г(лет) {day} дн {hour} ч {minute} мин {second} сек')
