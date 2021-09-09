bd836be20d35c74aeb6f5e8fdbb4ad4d88a7cc83
Anastasia Shaldova <83241853+AnastasiaShaldova@users.noreply.github.com>
2021-09-09 13:14:47 +0300

    Delete 2_4.py

--- 2_4.py
+++
@@ -1,5 +0,0 @@
-fellow_worker = ['инжинер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-for i in fellow_worker:
-    worker = i.split()[-1]
-    print(f'Привет, {worker.capitalize()}!')
-
