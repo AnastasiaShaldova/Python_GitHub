1b256fa83b5b6eaaad4451c08ed31a01a83c85b4
Anastasia Shaldova <83241853+AnastasiaShaldova@users.noreply.github.com>
2021-09-09 13:14:38 +0300

    Delete 2_2.py

--- 2_2.py
+++
@@ -1,9 +0,0 @@
-new_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-
-for n, i in enumerate(new_list):
-    if i.isdigit():
-        new_list[n] = f'"{i.zfill(2)}"'
-    elif i[1:].isdigit():
-        new_list[n] = f'"{i[0]}{i[1:].zfill(2)}"'
-
-print(' '.join(new_list))
